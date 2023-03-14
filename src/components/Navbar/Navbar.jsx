2e425cca5ee0881942b1e04d72b4fa6daec37a3c
ChAl76 <chornyalexander@gmail.com>
2023-03-14 14:04:47 +0200

    Navbar is done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { GitHamburgerMenu } from 'react-icons/gi';
+import React, { useState } from 'react';
+import { GiHamburgerMenu, GitHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
-
 import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
+const Navbar = () => {
+const [toggleMenu, SetToggleMenu] = useState(false);
+  return (
   <nav className='app__navbar'>
     <div className="app__navbar-logo">
       <img src={images.gericht} alt="app logo" />
@@ -17,7 +18,27 @@ const Navbar = () => (
       <li className="p__opensans"><a href="#awards">Awards</a></li>
       <li className="p__opensans"><a href="#contact">Contact</a></li>
     </ul>
-  </nav>
-);
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">Log In / Register</a>
+      <div />
+      <a href="/" className="p__opensans">Book Table</a>
+    </div>
+    <div className="app__navbar-smallscreen">
+       <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => SetToggleMenu(true)}/>
+        {toggleMenu && (
+        <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+          <MdOutlineRestaurantMenu fontSize={27} 
+          className="overlay__close" onClick={() => SetToggleMenu(false)}/>
+          <ul className="app__navbar-smallscreen_links">
+            <li className="p__opensans"><a href="#home">Home</a></li>
+            <li className="p__opensans"><a href="#about ">About</a></li>
+            <li className="p__opensans"><a href="#menu">Menu</a></li>
+            <li className="p__opensans"><a href="#awards">Awards</a></li>
+            <li className="p__opensans"><a href="#contact">Contact</a></li>
+          </ul>
+        </div>)}
+    </div>
+  </nav>)
+}
 
 export default Navbar;
